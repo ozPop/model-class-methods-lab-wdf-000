d4faf2893c383f7510a83b7a4ad9aeeb5e483bf2
ozPop <ozziepopovas@gmail.com>
2016-09-22 17:48:04 -0400

    add :sailors to captain model

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -11,7 +11,7 @@ class Captain < ActiveRecord::Base
 
     # joins("
     #   INNER JOIN boats
-    #     ON boats.captain_id = captains.id
+    #     ON boats.captabin_id = captains.id
     #   INNER JOIN boat_classifications
     #     ON boat_classifications.boat_id = boats.id
     #   INNER JOIN classifications
@@ -19,11 +19,33 @@ class Captain < ActiveRecord::Base
     #   ").where(classifications: { name: 'Catamaran' })
 
     # Soluton v3 using AR joins
-
+    # duplicated hey in hash
     # joins(boats: :boat_classifications, boats: :classifications).where(classifications: { name: 'Catamaran' })
   end
 
   def self.sailors
+    # Solution v1 using helper method in Boat class
+
+    # boats = Boat.sailboats
+    # captain_ids = boats.map(&:captain_id)
+    # where(id: captain_ids)
+
+    # Solution v2 using includes & Left outer Join
+
+    # includes(boats: :classifications).where(classifications:
+    # {
+    #   name: 'Sailboat'
+    # }).distinct
+
+    # Solution v3 using joins & Inner Join
+
+    joins(boats: :classifications).where(classifications:
+    {
+      name: 'Sailboat'
+    }).distinct
+  end
+
+  def self.talented_seamen
 
   end
 end
